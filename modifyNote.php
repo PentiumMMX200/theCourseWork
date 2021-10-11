5dd8fca6c3d663b47f3c7d1a65bafe434fc518ba
vano23201 <vanca624@gmail.com>
2021-10-11 12:28:30 +0300

    Added dump

--- modifyNote.php
+++ modifyNote.php
@@ -3,13 +3,13 @@
     $db->exec("set names utf8");
 
     if($_POST["action"] == "edit") {
-        $id = (int) $_POST["ID"];
+        $id = (int) $_POST["id"];
         $edit = $db->prepare("UPDATE notes set content=:content WHERE ID=:id");
         $edit->execute(array(":id"=>$id, ":content"=>"yolo)"));
     }
     
     if($_POST["action"] == "delete") {
-        $id = (int) $_POST["ID"];
+        $id = (int) $_POST["id"];
         $delete = $db->prepare("DELETE FROM notes WHERE ID=" . $id);
         $delete->execute();
     }
